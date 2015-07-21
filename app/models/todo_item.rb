f86cf7711e474d922558555d9d47c317d7bc6340
Anders Palm <ang.palm@hotmail.com>
2015-07-21 19:10:21 -0400

    Added mark as complete functionality

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,8 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list #created by todo_list:references when creating the rails g model
+
+  def completed?
+  	!completed_at.blank?
+  end
+
 end
