fc825e5975d4dbd8f773dd7cb5dd1a36ede9708f
Anders Palm <ang.palm@hotmail.com>
2015-07-22 16:44:36 -0400

    added css and javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// $('mark_complete').on('click', function(event) {
+// 	// var sound = new Audio("file.wav"); // buffers automatically when created
+// 	// sound.play();
+// 	$('.table_splitter').appendto
+
+// };
+
+var now = new Date();
+ 
+// Array list of days.
+var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+ 
+// Array list of months.
+var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+ 
+// Calculate the number of the current day in the week.
+var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+ 
+// Calculate four digit year.
+function fourdigits(number) {
+    return (number < 1000) ? number + 1900 : number;
+                                }
+ 
+// Join it all together
+today =  days[now.getDay()] + ", " +
+         months[now.getMonth()] + " " +
+         date + ", " +
+         (fourdigits(now.getYear())) ;
+ 
+// Print out the data.
+document.write(today);
